44eddb489b08c0bf95e13f9d4f78e2707b75b612
Andrei Warkentin <andrey.warkentin@gmail.com>
2018-07-02 00:57:46 -0400

    Update ArgExec.py

--- ArgExec.py
+++ ArgExec.py
@@ -2,8 +2,14 @@ import sys
 import console
 
 #
+# Note: completely unsafe, use at your own risk. This provides
+# no prompts!
+#
 # Example use (e.g. in a QR code):
 # pythonista://ios-py/ArgExec?action=run&args="console.hud_alert(\"hi there\")"
+#
+# https://meyerweb.com/eric/tools/dencoder/ to encode python before passing it as args string.
+# https://www.the-qrcode-generator.com/ to generate QR.
 #
 def main():
 	if len(sys.argv) == 1:
